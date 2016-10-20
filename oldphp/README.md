b5e6fac5e6131a20e74e1e569b1fb2cd8d77226b
Jennifer Nason <jnason@unknown9cefd5ffb9ae.att.net>
2016-10-20 12:20:18 -0700

    initial switch to marionette and npm

---
+++ oldphp/README.md
@@ -0,0 +1,6 @@
+# QizPop
+Rewrite of basic PHP quiz to use Backbone/Marionette/Boostrap (no longer Angular JS) on the front
+
+As you might notice, there is actually no framework in there yet. I really should commit my changes.
+
+In the meantime, it's a very basic pop quiz app, that barely works. The HTML/CSS is a terrible fail, and there is pretty much no error handling to speak of.
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/driver.js',
+
+  externals: {
+    'jquery': '$'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'underscore-template-loader'
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/static/js',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'underscore'
+    })
+  ],
+  resolve: {
+    modulesDirectories: [__dirname + '/node_modules'],
+    root: __dirname + '/app'
+  },
+  resolveLoader: {
+    root: __dirname + '/node_modules'
+  }
+};
